e1b44a8020e43a98ce283dfaaf0864db1e06519e
JFK93 <janknipping93@googlemail.com>
2021-02-03 23:39:35 +0100

    update

--- script/js/main.js
+++ script/js/main.js
@@ -1,20 +1,51 @@
+//Seitenbeschreibungstart
 function agreeCookies() {
     alert("Come to the darkside we have cookies!");
 }
 
-function checkCookies() {
-    var text = "darkside";
-    if (navigator.cookieEnabled == true) {
-        text = "Now you are on the darkside and you have cookies.";
-    } else {
-        text = "Come to the darkside we have cookies.";
-    }
-    document.getElementById("cookies").innerHTML = text;
+//Cookiezähler
+function init() {
+    var besuch = zaehlerstand();
+    var ausgabe = document.getElementById('info');
+    ausgabe.innerHTML = besuch;
 }
 
+function wertHolen() {
+    var Wert = "";
+    if (document.cookie) {
+        var Wertstart = document.cookie.indexOf("=") + 1;
+        var Wertende = document.cookie.indexOf(";");
+        if (Wertende == -1) {
+            Wertende = document.cookie.length;
+        }
+        Wert = document.cookie.substring(Wertstart, Wertende);
+    }
+    return Wert;
+}
+
+function wertSetzen(Bezeichner, Wert, Verfall) {
+    var jetzt = new Date();
+    var Auszeit = new Date(jetzt.getTime() + Verfall);
+    document.cookie = Bezeichner + "=" + Wert + "; expires=" + Auszeit.toGMTString() +
+        ";";
+}
+
+function zaehlerstand() {
+    var Verfallszeit = 1000 * 60 * 60 * 24 * 365;
+    var Anzahl = wertHolen();
+    var Zaehler = 0;
+    if (Anzahl != "") {
+        Zaehler = parseInt(Anzahl) || 0;
+    }
+    Zaehler = Zaehler + 1;
+    wertSetzen("Zaehler", Zaehler, Verfallszeit);
+    return (Zaehler);
+}
+window.addEventListener('DOMContentLoaded', init);
 var elem = document.getElementById("browser");
 elem.addEventListener('onclick', browserCheck);
 
+//Browserversionscheck
 function browserCheck() {
     var BrowserName = navigator.appName;
     var BrowserVersion = navigator.appVersion;
