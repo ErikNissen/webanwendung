92ea306c1764f74035aa843d98eed186ea2339b4
ErikNissen <erik.nissen@student.fh-kiel.de>
2021-02-04 09:55:56 +0100

    update

--- script/js/main.js
+++ script/js/main.js
@@ -3,29 +3,84 @@ function agreeCookies() {
     alert("Come to the darkside we have cookies!");
 }
 
-<<<<<<< HEAD
-=======
-<<
-<<
-<< < HEAD
->>>>>>> 6edb72a0ed2d837feef27a71a001f63316a8eb8c
-//Cookiezähler
-function init() {
+window.addEventListener('DOMContentLoaded', init);
+var elem = document.getElementById("browser");
+elem.addEventListener('onclick', browserCheck);
+
+//Browserversionscheck
+function browserCheck() {
+    var BrowserName = navigator.appName;
+    var BrowserVersion = navigator.appVersion;
+    text2 = "Sie verwendest den <span>" + BrowserName +
+        "<\/span>, in der Version <span>" + BrowserVersion + "<\/span>";
+    var browserID = document.getElementById("browserID");
+    browserID.innerHTML = text2;
+}
+
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF6051";
+ctx.fillRect(5, 5, 190, 90);
+
+//AJAX
+function loadDoc() {
+    let jsn = new XMLHttpRequest();
+    jsn.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let jsonresp = JSON.parse(this.responseText);
+            document.getElementById("iban").innerHTML = jsonresp.IBAN[Math.floor((Math.random() * 6))];
+            console.log("Function loadDoc(): Done");
+        }
+    };
+    jsn.open("GET", "http://localhost/script/js/ajax.json", true);
+    jsn.send();
+}
+
+//Formular leeren
+function resetForm(id) {
+    document.getElementById(id).reset();
+}
+
+//Cookies
+function setCookie(name, val, exd){
+    if(getCookie(name) >= 1){
+        let value = getCookie(name) + 1;
+        let d = new Date();
+        d.setTime(d.getTime() + (exd*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = name + "=" + value + ";" + expires + ";path=/";
+        document.getElementById("info").innerHTML = value;
+
+    }else{
+        let d = new Date();
+        d.setTime(d.getTime() + (exd*24*60*60*1000));
+        let expires = "expires="+ d.toUTCString();
+        document.cookie = name + "=" + val + ";" + expires + ";path=/";
+        document.getElementById("info").innerHTML = val;
+    }
+    
+}
+
+function getCookie(cnm) {
+    let dat = document.cookie.split(";");
+    let val = dat[1].split("=")[1];
+    return parseInt(val);
+}
+
+function checkCookie(cnm) {
+    var user=getCookie(cnm);
+    if (user != "") {
+      return 1;
+    } else {
+         return 0;
+    }
+  }
+  function init() {
     var besuch = zaehlerstand();
     var ausgabe = document.getElementById('info');
     ausgabe.innerHTML = besuch;
 }
 
-<<<<<<< HEAD
-=======
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-
->>>>>>> 6edb72a0ed2d837feef27a71a001f63316a8eb8c
 function wertHolen() {
     var Wert = "";
     if (document.cookie) {
@@ -38,20 +93,6 @@ function wertHolen() {
     }
     return Wert;
 }
-<<<<<<< HEAD
-=======
-
-function wertSetzen(Bezeichner, Wert, Verfall) {
-    var jetzt = new Date();
-    var Auszeit = new Date(jetzt.getTime() + Verfall);
-    document.cookie = Bezeichner + "=" + Wert + "; expires=" + Auszeit.toGMTString() +
-        ";";
-} ===
-===
-= >>>
->>>
-> 8e8821 a2925049d03117a872b191c40123e38758
->>>>>>> 6edb72a0ed2d837feef27a71a001f63316a8eb8c
 
 function wertSetzen(Bezeichner, Wert, Verfall) {
     var jetzt = new Date();
@@ -71,19 +112,7 @@ function zaehlerstand() {
     wertSetzen("Zaehler", Zaehler, Verfallszeit);
     return (Zaehler);
 }
-window.addEventListener('DOMContentLoaded', init);
-var elem = document.getElementById("browser");
-elem.addEventListener('onclick', browserCheck);
 
-//Browserversionscheck
-function browserCheck() {
-    var BrowserName = navigator.appName;
-    var BrowserVersion = navigator.appVersion;
-    text2 = "Sie verwendest den <span>" + BrowserName +
-        "<\/span>, in der Version <span>" + BrowserVersion + "<\/span>";
-    var browserID = document.getElementById("browserID");
-    browserID.innerHTML = text2;
-}
 //Canvas
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
@@ -118,60 +147,3 @@ function draw() {
 
 setInterval(draw, 10);
 
-//AJAX
-function loadDoc() {
-    let jsn = new XMLHttpRequest();
-    jsn.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let jsonresp = JSON.parse(this.responseText);
-            document.getElementById("iban").innerHTML = jsonresp.IBAN[Math.floor((Math.random() * 6))];
-            console.log("Function loadDoc(): Done");
-        }
-    };
-    jsn.open("GET", "http://localhost/script/js/ajax.json", true);
-    jsn.send();
-}
-
-//Formular leeren
-function resetForm(id) {
-    document.getElementById(id).reset();
-<<<<<<< HEAD
-}
-=======
-}
-
-//Cockies
-function setCookie(name, val, exd){
-    if(getCookie(name) >= 1){
-        let value = getCookie(name) + 1;
-        let d = new Date();
-        d.setTime(d.getTime() + (exd*24*60*60*1000));
-        let expires = "expires="+ d.toUTCString();
-        document.cookie = name + "=" + value + ";" + expires + ";path=/";
-        document.getElementById("info").innerHTML = value;
-
-    }else{
-        let d = new Date();
-        d.setTime(d.getTime() + (exd*24*60*60*1000));
-        let expires = "expires="+ d.toUTCString();
-        document.cookie = name + "=" + val + ";" + expires + ";path=/";
-        document.getElementById("info").innerHTML = val;
-    }
-    
-}
-
-function getCookie(cnm) {
-    let dat = document.cookie.split(";");
-    let val = dat[1].split("=")[1];
-    return parseInt(val);
-}
-
-function checkCookie(cnm) {
-    var user=getCookie(cnm);
-    if (user != "") {
-      return 1;
-    } else {
-         return 0;
-    }
-  }
->>>>>>> 73ec8bfab128b964edd917f6ffc86ce10dcf555d
